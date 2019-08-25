904eebed7f3ac48ed2f26a156285f7c6fe6ce0cd
Alex Young <alex.buzov@icloud.com>
2019-08-25 15:39:46 -0700

    watchos target

---
+++ Examples/GridExamples watchOS WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GridExamples watchOS WatchKit Extension
+//
+//  Created by Alex Young on 2019-08-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Examples/GridExamples watchOS WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  GridExamples watchOS WatchKit Extension
+//
+//  Created by Alex Young on 2019-08-25.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
