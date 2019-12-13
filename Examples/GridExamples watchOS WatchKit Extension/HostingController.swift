f7d1547f47f975824b2dafa15738f6f5f393ab27
Alex Young <alex.buzov@icloud.com>
2019-12-12 22:59:11 -0800

    cleanup (#41)

--- Examples/GridExamples watchOS WatchKit Extension/HostingController.swift
+++
@@ -1,9 +0,0 @@
-import WatchKit
-import Foundation
-import SwiftUI
-
-class HostingController: WKHostingController<ContentView> {
-    override var body: ContentView {
-        return ContentView()
-    }
-}
--- Examples/GridExamples/Item.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct Item: Identifiable {
-    let number: Int
-    let id: UUID = UUID()
-    let color: Color = .random
-}
